db4465ed8672ec1d35133a5246b27c84f42059dc
fox <fonadgergo@hotmail.com>
2023-09-21 09:42:27 +0200

    added missing bug fix parts

--- lib/repositories/user_repository/user_repository_impl.dart
+++ lib/repositories/user_repository/user_repository_impl.dart
@@ -6,9 +6,18 @@ import 'package:state_machine/model/user_model/users.dart';
 import 'package:state_machine/repositories/user_repository/user_repository.dart';
 
 class UserRepositoryImpl extends UserRepository {
-  Box userBox = Hive.box('user_box');
-  Users? users = box.get('user');
+  late Box?   userBox;
+  late Users? users;
 
+UserRepositoryImpl(){
+  _init();
+}
+
+void _init() async {
+  Hive.registerAdapter(UsersAdapter());
+  userBox = await Hive.box('user_box');
+  users   = await userBox!.get('user');
+}
 
   StreamController<List<Map<String, dynamic>>> usersStream = StreamController();
 
@@ -17,8 +26,8 @@ class UserRepositoryImpl extends UserRepository {
 
   @override
   List<Map<String, dynamic>>? getUsers(){
-    List<Map<String, dynamic>> users = userBox.keys.map((key) {
-      final value = userBox.get(key);
+    List<Map<String, dynamic>> users = userBox!.keys.map((key) {
+      final value = userBox!.get(key);
       return {"key": key, "name": value["name"], "age": value["age"]};
     }).toList();
     return users;
@@ -27,7 +36,7 @@ class UserRepositoryImpl extends UserRepository {
   @override
   Future<List<Map<String, dynamic>>> createUser(
       Map<String, dynamic> newUser) async {
-    await userBox.add(newUser);
+    await userBox!.add(newUser);
     usersStream.add( await getUsers()!);
     return getUsers()!;
   }
@@ -35,15 +44,15 @@ class UserRepositoryImpl extends UserRepository {
   @override
   Future<List<Map<String, dynamic>>> updateUser(
       int key, Map<String, dynamic> user) async {
-    await userBox.put(key, user);
+    await userBox!.put(key, user);
     usersStream.add( await getUsers()!);
     return getUsers()!;
   }
 
   @override
   Future<List<Map<String, dynamic>>?>? deleteUser(int key) async {
-    await userBox.delete(key);
-    if (userBox.isEmpty){
+    await userBox!.delete(key);
+    if (userBox?.isEmpty??true){
       return Future.value(null);
     }
     final users = await getUsers()!;
